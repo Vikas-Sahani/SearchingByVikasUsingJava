5b72b4cd8c45feef0e0194fef362532a513b1c6b
Vikas <102462411+Vikas-Sahani@users.noreply.github.com>
2023-06-22 10:34:11 +0530

    adding LeecodeFolder

---
+++ LeetCode/852PeakIdxInMountainArr.js
@@ -0,0 +1,19 @@
+// 852. Peak Index in a Mountain Array => https://leetcode.com/problems/peak-index-in-a-mountain-array/description/
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var peakIndexInMountainArray = function (arr) {
+  let start = 0;
+  let end = arr.length - 1;
+
+  while (start < end) {
+    let mid = Math.floor(start + (end - start) / 2);
+    if (arr[mid] < arr[mid + 1]) {
+      start = mid + 1;
+    } else {
+      end = mid; // all the left element of the array is smaller so it can be the posible answer.
+    }
+  }
+  return start;
+};
